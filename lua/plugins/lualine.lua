448132faf4678f151681e17729791ac101cb0e7d
Reinhard Staudacher <rstaudacher@github.com>
2024-05-22 11:04:45 +0200

    Add lua line and remove unnecessary folder

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  lazy = false,
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'dracula',
+        section_separators = '',
+      },
+    }
+  end,
+}
